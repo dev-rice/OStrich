93bb1e4ae4545d0c3aabded3d526022ade3a56e1
Chris Rice <chrisrice93@gmail.com>
2015-09-26 17:21:01 -0600

    weird

--- src/addressing_test.asm
+++ src/addressing_test.asm
@@ -6,10 +6,38 @@ mov bx, my_string
 mov al, [bx]
 int 0x10
 
+add bx, 1
+mov al, [bx]
+int 0x10
+
+add bx, 1
+mov al, [bx]
+int 0x10
+
+add bx, 1
+mov al, [bx]
+int 0x10
+
+add bx, 1
+mov al, [bx]
+int 0x10
+
+add bx, 1
+mov al, [bx]
+int 0x10
+
+add bx, 1
+mov al, [bx]
+int 0x10
+
+add bx, 1
+mov al, [bx]
+int 0x10
+
 jmp $
 
 my_string:
-    db 'X'
+    db "The quick brown fox jumps over the lazy dog"
 
 times 510-($-$$) db 0
 dw 0xaa55
--- src/hello_world.asm
+++ src/hello_world.asm
@@ -1,16 +1,12 @@
 mov ah, 0x0e
-mov al, [0x0000]
+
+mov al, 'A'
+int 0x10
+
+mov al, 'B'
 int 0x10
 
 jmp $
-
-%include "src/print_string.asm"
-
-HELLO_MSG:
-    db 'Hello, World!', 0
-
-GOODBYE_MSG:
-    db 'Goodbye!', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
